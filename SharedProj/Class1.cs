b39d4689f4f336af5634036fece17ea5923c88a6
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-21 00:24:05 +0400

    Add Product class

--- SharedProj/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SharedProj;
-
-public class Class1
-{
-
-}
---
+++ SharedProj/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharedProj;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+    [MaxLength(20)]
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public DateTime CreationalDate { get; set; }
+    public double Price { get; set; }
+}
