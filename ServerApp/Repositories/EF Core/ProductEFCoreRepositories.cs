83ef95d5f9eb01be8c3cfc8762ba416bcd237ef2
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-21 03:10:58 +0400

    Release elementary CRUD

---
+++ ServerApp/Repositories/EF Core/ProductEFCoreRepositories.cs
@@ -0,0 +1,48 @@
+﻿using ServerApp.Repositories.Base;
+using ServerApp.Repositories.EF_Core.DbContext;
+using SharedProj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp.Repositories.EF_Core;
+
+public class ProductEFCoreRepositories : IProductRepository
+{
+    private readonly ProductDbContext dbcontext;
+
+    public ProductEFCoreRepositories() 
+    {  
+        dbcontext = new ProductDbContext(); 
+    }
+    public void Delete(int id)
+    {
+        Product product = GetById(id);
+
+        dbcontext.Remove<Product>(product);
+        dbcontext.SaveChanges();
+    }
+
+    public IEnumerable<Product> GetAll() => dbcontext.Products;
+
+    public Product GetById(int id)
+    {
+        return dbcontext.Products.FirstOrDefault(p => p.Id == id)
+            ?? throw new ArgumentNullException("There is no product with such ID");
+    }
+
+    public void Post(Product product)
+    {
+        dbcontext.Products.Add(product);
+        dbcontext.SaveChanges();
+    }
+
+    public void Update(int id, Product product)
+    {
+        Product currentProduct = GetById(id);
+        currentProduct = product;
+        dbcontext.SaveChanges();
+    }
+}
