ca04c0ce6d1d4bf04ff5104dd55088c77d85fbb8
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-21 02:51:30 +0400

    Add init migration

---
+++ ServerApp/Migrations/20231120223109_init.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationalDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- ServerApp/Repositories/EF Core/DbContext/ProductDbContext.cs
+++ ServerApp/Repositories/EF Core/DbContext/ProductDbContext.cs
@@ -9,19 +9,19 @@ namespace ServerApp.Repositories.EF_Core.DbContext;
 using Microsoft.EntityFrameworkCore;
 using SharedProj;
 
-public class ProductDbContextcs : DbContext
+public class ProductDbContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
 
-    public ProductDbContextcs()
-    {
-        this.Database.EnsureCreated();
-    }
+    //public ProductDbContext()
+    //{
+    //    this.Database.EnsureCreated();
+    //}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
 
-        optionsBuilder.UseSqlServer(connectionString: "Server=localhost;Database=Shop;TrustServerCertificate=True");//User Id=admin;Password=admin;
+        optionsBuilder.UseSqlServer(connectionString: @"Server=localhost;Database=Shop;Trusted_Connection=True;TrustServerCertificate=True");//User Id=admin;Password=admin;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
