7539aee8af2e440dc401c29cf3e44cca6711ad25
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-21 01:41:35 +0400

    Add Repositories

---
+++ ServerApp/Repositories/Base/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp.Repositories.Base;
+
+using SharedProj;
+
+public interface IProductRepository
+{
+    public IEnumerable<Product> GetAll();
+    public Product GetById(int id);
+    public void Post(Product product);
+    public void Update(int id, Product product);
+    public void Delete(int id);
+}
