525b0d54a4407b30d2d25c2c08c040d68b47ce1f
makedoniaz <jafar.sadig04@gmail.com>
2023-11-21 23:06:10 +0400

    Add ViewModelBase and MainViewModel

---
+++ ClientApp/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ClientApp.ViewModels.Base;
+
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void PropertyChangeMethod<T>(out T field, T value, [CallerMemberName] string propName = "")
+    {
+        field = value;
+
+        if (this.PropertyChanged != null)
+            this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
---
+++ ClientApp/ViewModels/Main/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ClientApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp.ViewModels.Main;
+
+public class MainViewModel : ViewModelBase
+{
+    #region Fields
+
+    private ViewModelBase? activeViewModel;
+    public ViewModelBase? ActiveViewModel
+    {
+        get => activeViewModel;
+        set => base.PropertyChangeMethod(out activeViewModel, value);
+    }
+
+    #endregion
+
+
+    public MainViewModel()
+    {
+
+    }
+}
