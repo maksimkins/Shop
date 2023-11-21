1fc0ab89899f2113f1309b52843eadaa93570d0b
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-21 16:10:16 +0400

    Create HttpServer class

---
+++ ServerApp/HttpServer/HttpServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp.HttpServer;
+
+public class HttpServer
+{
+    private readonly HttpListener listener;
+
+    public HttpServer(int port)
+    {
+        listener = new HttpListener();
+        listener.Prefixes.Add($"http://*:{port}/");
+        listener.Start();
+    }
+
+   
+
+}
