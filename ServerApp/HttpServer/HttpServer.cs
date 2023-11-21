1b1c12e5015b6e9a62ac4afa9aea984dd7dcd7ea
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-21 16:30:42 +0400

    Create base methods

--- ServerApp/HttpServer/HttpServer.cs
+++ ServerApp/HttpServer/HttpServer.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ServerApp.HttpServer;
 
-public class HttpServer
+public class HttpServer  
 {
     private readonly HttpListener listener;
 
@@ -18,6 +18,35 @@ public class HttpServer
         listener.Start();
     }
 
-   
+    public async void HttpListen()
+    {
+        while (true)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            if (context is null)
+            {
+                break;
+            }
+
+            RequestHandle(context);
+        }
+    }
+
+    public async void RequestHandle(HttpListenerContext context)// TO DO
+                                                                // create logic that depends on Request (will check reques method) and call specific method
+    {
+
+        Console.WriteLine($"{context.User} connected to server");
+
+        await ResponseSender(context);
+    }
+
+    public async Task ResponseSender(HttpListenerContext context)// instance of specific method
+    {
+        using var writer = new StreamWriter(context.Response.OutputStream);
+        context.Response.StatusCode = 200;
+        await writer.WriteLineAsync("you succesfully conected to the server");
+    }
 
 }
