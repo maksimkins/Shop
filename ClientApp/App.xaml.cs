37376b7d9aad854f5f855ab0272200a3de67bd2a
makedoniaz <jafar.sadig04@gmail.com>
2023-11-21 23:24:11 +0400

    Add Homepage and set application startup in App.xaml.cs

--- ClientApp/App.xaml.cs
+++ ClientApp/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ClientApp.ViewModels.Base;
+using ClientApp.ViewModels.Main;
+using ClientApp.ViewModels.Pages;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
@@ -13,5 +17,22 @@ namespace ClientApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            this.Start();
+
+            base.OnStartup(e);
+        }
+
+        private void Start()
+        {
+            var mainView = new MainWindow();
+            var mainViewModel = new MainViewModel();
+
+            mainView.DataContext = mainViewModel;
+            mainViewModel.ActiveViewModel = new HomeViewModel();
+
+            mainView.ShowDialog();
+        }
     }
 }
---
+++ ClientApp/ViewModels/Pages/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ClientApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp.ViewModels.Pages;
+
+public class HomeViewModel : ViewModelBase
+{
+
+}
