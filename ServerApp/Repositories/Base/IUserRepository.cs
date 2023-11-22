1767fef678816b6568d4c3ae6b5ab44f78691de5
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-22 23:25:16 +0400

    Add IUserRepository interface

---
+++ ServerApp/Repositories/Base/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using SharedProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp.Repositories.Base;
+
+public interface IUserRepository
+{
+    public bool IsRegistered(string login, string password);
+    public void Register(User product);
+}
