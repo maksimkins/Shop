1d6af65732ef5d10a8969aea33b52622349aad39
makedoniaz <jafar.sadig04@gmail.com>
2023-11-23 22:31:05 +0400

    Add MyHttpClient and Implement Get method

---
+++ ClientApp/Utilities/MyHttpClient/MyHttpClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClientApp.Utilities.MyHttpClient;
+
+public class MyHttpClient
+{
+    private readonly HttpClient _httpClient;
+
+    public MyHttpClient()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public static StringContent ConvertObject<T>(T obj)
+    {
+        return new StringContent(JsonSerializer.Serialize(obj), Encoding.UTF8, "application/json");
+    }
+
+    public Task<HttpResponseMessage> GetAsync(string uri)
+    {
+       return _httpClient.GetAsync(uri);
+    }
+
+    public Task<HttpResponseMessage> PostAsync<T>(string uri, T obj) {
+        var content = ConvertObject(obj);
+        return _httpClient.PostAsync(uri, content);
+    }
+
+    public Task<HttpResponseMessage> PutAsync<T>(string uri, T obj)
+    {
+        var content = ConvertObject(obj);
+        return _httpClient.PutAsync(uri, content);
+    }
+}
--- ClientApp/ViewModels/Pages/HomeViewModel.cs
+++ ClientApp/ViewModels/Pages/HomeViewModel.cs
@@ -1,13 +1,57 @@
-﻿using ClientApp.ViewModels.Base;
+﻿using ClientApp.Utilities.MyHttpClient;
+using ClientApp.ViewModels.Base;
+using ClientApp.ViewModels.Main;
+using SharedProj.Models;
+using SimpleInjector;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ClientApp.ViewModels.Pages;
 
 public class HomeViewModel : ViewModelBase
 {
+    #region Fields
 
+    private readonly MyHttpClient _httpClient;
+
+    public ObservableCollection<Product> Products { get; set; }
+
+    #endregion
+
+
+    #region Constructor
+    public HomeViewModel()
+    {
+        _httpClient = App.Container.GetInstance<MyHttpClient>();
+        this.Products = new ObservableCollection<Product>();
+        GetProducts();
+    }
+    #endregion
+
+
+    #region Methods
+    public async void GetProducts()
+    {
+        var response = await _httpClient.GetAsync("http://localhost:8080/Product");
+
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            return;
+    
+        var json = await response.Content.ReadAsStringAsync();
+        var products = JsonSerializer.Deserialize<ObservableCollection<Product>>(json) ??
+            throw new Exception();
+
+        foreach (var product in products)
+            this.Products.Add(product);
+    }
+
+    #endregion
 }
