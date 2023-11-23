c4e18ae4e70f9cd5501fd740c55474b9434555a1
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-23 21:04:28 +0400

    Add IRequestHandler

---
+++ ServerApp/HttpServer/RequestHandlers/Base/IRequestHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp.HttpServer.RequestHandlers.Base;
+
+public interface IHttpRequestHandler
+{
+    public void SetNextHandler(IHttpRequestHandler requestHandler);
+    public void RequestHandle(HttpListenerContext context);
+}
