8361b80f5702dd3754d23495f712fdfd7fc8c5bb
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-25 00:17:21 +0400

    Add ProductDTO

---
+++ SharedProj/Models/ProductDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedProj.Models;
+
+public class ProductDTO
+{
+    string? Title { get; set; }
+    string? Text { get; set; }
+    double? PriceFrom { get; set; }
+    double? PriceTo { get; set; }
+}
