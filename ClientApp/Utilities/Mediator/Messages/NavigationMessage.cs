470422ba8cde6953a152b74423b13febeb90c3d9
makedoniaz <jafar.sadig04@gmail.com>
2023-11-25 21:25:02 +0400

    Add 'Home' button functional in HomeViewModel

--- ClientApp/Utilities/Mediator/Messages/NavigationMessage.cs
+++ ClientApp/Utilities/Mediator/Messages/NavigationMessage.cs
@@ -1,4 +1,5 @@
-﻿using ClientApp.ViewModels.Base;
+﻿using ClientApp.Utilities.Mediator.Messages.Interfaces;
+using ClientApp.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,13 @@ using System.Threading.Tasks;
 
 namespace ClientApp.Utilities.Mediator.Messages;
 
-public class NavigationMessage
+public class NavigationMessage : IMessage
 {
     public readonly ViewModelBase DestinationViewModel;
 
     public NavigationMessage(ViewModelBase destinationViewModel)
     {
         this.DestinationViewModel = destinationViewModel;
+        this.DestinationViewModel.RefreshViewModel();
     }
 }
--- ClientApp/ViewModels/Main/MainViewModel.cs
+++ ClientApp/ViewModels/Main/MainViewModel.cs
@@ -1,4 +1,8 @@
-﻿using ClientApp.ViewModels.Base;
+﻿using ClientApp.Utilities.Command.Base;
+using ClientApp.Utilities.Mediator.Interfaces;
+using ClientApp.Utilities.Mediator.Messages;
+using ClientApp.ViewModels.Base;
+using ClientApp.ViewModels.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +15,9 @@ public class MainViewModel : ViewModelBase
 {
     #region Fields
 
+    private readonly IMessenger _messenger;
+
+
     private ViewModelBase? activeViewModel;
     public ViewModelBase? ActiveViewModel
     {
@@ -21,8 +28,30 @@ public class MainViewModel : ViewModelBase
     #endregion
 
 
-    public MainViewModel()
+    #region Constructor
+    public MainViewModel(IMessenger messenger)
     {
+        _messenger = messenger;
 
+        _messenger.Subscribe<NavigationMessage>((message) =>
+        {
+            if (message is NavigationMessage navigationMessage)
+            {
+                this.ActiveViewModel = navigationMessage.DestinationViewModel;
+                this.ActiveViewModel.RefreshViewModel();
+            }
+        });
     }
+    #endregion
+
+    #region Commands
+    private CommandBase? homeCommand;
+    public CommandBase HomeCommand => this.homeCommand ??= new CommandBase(
+            execute: () => {
+                _messenger.Send(new NavigationMessage(App.Container.GetInstance<HomeViewModel>()));
+            },
+            canExecute: () => true);
+
+    #endregion
+
 }
